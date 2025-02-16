79ff87460eba1065154889cea47fc7613132bdad
Yaya Dia <yayadia@MacBook-Pro-de-Yaya.local>
2025-02-16 14:13:09 +0000

    first commit

--- models/UserManager.php
+++ models/UserManager.php
@@ -1,8 +1,12 @@
 <?php
+
 function infoUser(){
 require_once ('../models/bd/Model.php');
     $info_use = recTableId ( $bd, 'user' , $_SESSION["id"]);
     return $info_use ;
 }
-
+//verifie si user a une boutique
+function infoUserBoutiqu(){
+    require_once ('../models/bd/Model.php');
+}
     ?>
