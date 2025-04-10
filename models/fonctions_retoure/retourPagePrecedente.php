eccf5ebe3ecc81191d4a9918b33626b56030198b
Paul 2617 <paulpassanikone@gmail.com>
2025-04-10 12:57:57 +0000

    ffff

--- models/fonctions_retoure/retourPagePrecedente.php
+++ models/fonctions_retoure/retourPagePrecedente.php
@@ -6,11 +6,12 @@ function retourPagePrecedente($fallback = '/Kephale/accueil') {
         $nom_COOKIE = $_GET["url"];
         $valeur = $urlPrecedente;
         $expiration = time() + (30 * 24 * 60 * 60); // 30 jours
-        if(isset($_COOKIE[$nom_COOKIE])){
-        echo "<a class ='lin_connect' href=\"$_COOKIE[$nom_COOKIE]\"><img src='public/asset/_icone/retoure.svg' ></a>";
-        }else{
+        if(empty($_COOKIE[$nom_COOKIE])){
             setcookie($nom_COOKIE, $valeur, $expiration);
-            header("Refresh: 1");
+            header("Refresh: 0");
+        }
+        if(isset($_COOKIE[$nom_COOKIE])){
+            echo "<a class ='lin_connect' href=\"$_COOKIE[$nom_COOKIE]\"><img src='public/asset/_icone/retoure.svg' ></a>";
         }
         //
 
